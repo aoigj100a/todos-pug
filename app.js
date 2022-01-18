69cd453da4fbc18da3e608b099bfbdd47362b7e0
aoigj100a <aoigj100@gmail.com>
2022-01-18 19:04:34 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const routes = require("./routes");
+require("./config/mongoose");
 
 const app = express();
+app.set("view engine", "pug");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
-app.set("view engine", "pug");
-
-mongoose.connect("mongodb://localhost/todo-list", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
-
 app.use(routes);
 
 app.listen(3000, () => {
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,21 +1,9 @@
-const mongoose = require("mongoose");
 const Todo = require("../todo");
-
-mongoose.connect("mongodb://localhost/todo-list", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
+const db = require("../../config/mongoose");
 
 db.once("open", () => {
-  console.log("mongodb connected!");
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: "name-" + i });
   }
   console.log("done");
-});
-
-db.on("error", () => {
-  console.log("mongodb error!");
 });
