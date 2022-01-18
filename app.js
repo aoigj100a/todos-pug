cf02264d30f3bb05c2f11509ccb6326062022ff2
aoigj100a <aoigj100@gmail.com>
2022-01-18 16:38:36 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ db.once("open", () => {
 app.get("/", (req, res) => {
   Todo.find()
     .lean()
+    .sort({ _id: "asc" })
     .then((todos) => res.render("index", { todos }))
     .catch((error) => console.log(error));
 });
