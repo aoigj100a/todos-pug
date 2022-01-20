15aa356d1b6d1ed9e078a0b0a8522071d8f4ee49
aoigj100a <aoigj100@gmail.com>
2022-01-20 19:40:00 +0800

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,9 +1,29 @@
 const Todo = require("../todo");
+const User = require("../user");
 const db = require("../../config/mongoose");
 
+const SEED_USER = {
+  name: "root",
+  email: "root@example.com",
+  password: "12345678",
+};
+
 db.once("open", () => {
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: "name-" + i });
-  }
-  console.log("done");
+  User.create({
+    name: SEED_USER.name,
+    email: SEED_USER.email,
+    password: SEED_USER.password,
+  })
+    .then((user) => {
+      const userId = user._id;
+      return Promise.all(
+        Array.from({ length: 10 }, (_, i) =>
+          Todo.create({ name: `name-${i}`, userId })
+        )
+      );
+    })
+    .then(() => {
+      console.log("done.");
+      process.exit();
+    });
 });
